96740671333522eff6a364e8a08ff20ab09766e2
Kyletho2020 <kyle.tho@icloud.com>
2025-09-08 15:15:22 -0700

    Add shop location dropdown with default

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ const App: React.FC = () => {
     contactName: '',
     siteAddress: '',
     sitePhone: '',
-    shopLocation: '',
+    shopLocation: 'Shop',
     scopeOfWork: '',
     email: '',
     equipmentRequirements: initialEquipmentRequirements
