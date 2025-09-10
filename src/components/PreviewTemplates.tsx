f094ac6a68c990e87b12950ff86488cef5d4d6c7
Kyletho2020 <kyle.tho@icloud.com>
2025-09-10 08:00:30 -0700

    Separate site address line in scope template

--- src/components/PreviewTemplates.tsx
+++ src/components/PreviewTemplates.tsx
@@ -90,7 +90,8 @@ export const generateScopeTemplate = (
           .join('\n')}\n`
       : ''
 
-  return `Mobilize crew and Omega Morgan equipment to site: ${siteAddress}
+  return `Mobilize crew and Omega Morgan equipment to site:
+${siteAddress}
 
 ${contactName}
 ${phone}
